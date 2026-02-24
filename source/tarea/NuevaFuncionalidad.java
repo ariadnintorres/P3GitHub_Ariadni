22e4bbed66e0157e11dc0b861f1530daa1d280d4
Nani <ariadnintorres@gmail.com>
2026-02-24 02:27:19 +0100

    Update NuevaFuncionalidad.java
    --nueva funcion

--- source/tarea/NuevaFuncionalidad.java
+++ source/tarea/NuevaFuncionalidad.java
@@ -1,5 +1,19 @@
 package tarea;
 
-public class NuevaFuncionalidad {
+import java.util.Scanner;
 
+public class NuevaFuncionalidad {
+	public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        
+        System.out.println("--- Nueva Funcionalidad: Calculadora de Descuentos ---");
+        System.out.print("Introduce el precio del producto: ");
+        double precio = sc.nextDouble();
+        double precioFinal = precio * 0.85;
+        
+        System.out.println("Precio original: " + precio + "€");
+        System.out.println("Precio con descuento (15%): " + precioFinal + "€");
+        
+        sc.close();
+    }
 }
